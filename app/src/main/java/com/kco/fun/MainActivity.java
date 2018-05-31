7ce98f5e4ef1887d671d0a23f1b4615034343f78
lvsw <lsw@eeepay.cn>
2018-05-31 14:42:31 +0800

    获取图片测试

--- app/src/main/java/com/kco/fun/MainActivity.java
+++ app/src/main/java/com/kco/fun/MainActivity.java
@@ -1,55 +1,25 @@
 package com.kco.fun;
 
 import android.content.Intent;
-import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import com.jph.takephoto.app.TakePhotoActivity;
-import com.jph.takephoto.model.TImage;
-import com.jph.takephoto.model.TResult;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends TakePhotoActivity {
-    private static final String TAG = "MainActivity";
-    private CustomHelper customHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View contentView = LayoutInflater.from(this).inflate(R.layout.common_layout, null);
-        setContentView(contentView);
-        customHelper = CustomHelper.of(contentView);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
-    public void onClick(View view) {
-        customHelper.onClick(view, getTakePhoto());
-    }
-
-    @Override
-    public void takeSuccess(TResult result) {
-        super.takeSuccess(result);
-        Log.d(TAG, "single -> " + result.getImage().getOriginalPath());
-        for (TImage image : result.getImages()){
-            Log.d(TAG, "bat ->" + image.getOriginalPath());
-        }
-        showImg(result.getImages());
-    }
-
-    @Override
-    public void takeFail(TResult result, String msg) {
-        super.takeFail(result, msg);
-    }
-
-    @Override
-    public void takeCancel() {
-        super.takeCancel();
-    }
-
-    private void showImg(ArrayList<TImage> images) {
-        Intent intent = new Intent(this, ResultActivity.class);
-        intent.putExtra("images", images);
+    @OnClick(R.id.start_simple)
+    public void startSimple(View view){
+        Intent intent = new Intent(this, SimpleActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/kco/fun/SimpleActivity.java
@@ -0,0 +1,55 @@
+package com.kco.fun;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.model.TImage;
+import com.jph.takephoto.model.TResult;
+
+import java.util.ArrayList;
+
+public class SimpleActivity extends TakePhotoActivity {
+    private static final String TAG = "MainActivity";
+    private CustomHelper customHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.common_layout, null);
+        setContentView(contentView);
+        customHelper = CustomHelper.of(contentView);
+    }
+
+    public void onClick(View view) {
+        customHelper.onClick(view, getTakePhoto());
+    }
+
+    @Override
+    public void takeSuccess(TResult result) {
+        super.takeSuccess(result);
+        Log.d(TAG, "single -> " + result.getImage().getOriginalPath());
+        for (TImage image : result.getImages()){
+            Log.d(TAG, "bat ->" + image.getOriginalPath());
+        }
+        showImg(result.getImages());
+    }
+
+    @Override
+    public void takeFail(TResult result, String msg) {
+        super.takeFail(result, msg);
+    }
+
+    @Override
+    public void takeCancel() {
+        super.takeCancel();
+    }
+
+    private void showImg(ArrayList<TImage> images) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("images", images);
+        startActivity(intent);
+    }
+}
